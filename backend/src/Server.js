d4eee8484370604692d2a1265425e20fa2a4e25b
HAMYL-Aththnayaka <Aththanayakayasas@gmail.com>
2025-09-24 23:46:07 +0530

    Add backend files

---
+++ backend/src/Server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT | 3000
+
+app.listen(port ,()=>{
+    console.log(`The Server is Running on ${port}`)
+})
